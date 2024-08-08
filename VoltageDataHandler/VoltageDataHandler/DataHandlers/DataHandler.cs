ff28f908f6057eb27d4e8addfc97ac028a949fff
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-08-08 23:39:43 +0700

    Delete header

---
+++ VoltageDataHandler/VoltageDataHandler/DataHandlers/DataHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoltageDataHandler.DataHandlers
+{
+    public class DataHandler
+    {
+
+        public Func<List<string>, List<string>> DeleteHeader = data => data.Skip(4).ToList();
+
+    }
+}
--- VoltageDataHandler/VoltageDataHandler/Program.cs
+++ VoltageDataHandler/VoltageDataHandler/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace VoltageDataHandler
+﻿using System.Diagnostics;
+using VoltageDataHandler.DataHandlers;
+using VoltageDataHandler.FileHandlers;
+
+namespace VoltageDataHandler
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var handler = new TxtFileHandler();
+            var data = handler.ReadFiles("C:/Users/carna/source/repos/IiyaMezer/VoltageAnalyze/VoltageDataHandler/ZAP0138.TXT");
+
+
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
         }
     }
 }
