b51c1a1b54eb67e086bdc79b6b6b6642c597454b
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-08-08 07:29:21 +0700

    Интерфейсы и модели

---
+++ VoltageDataHandler/VoltageDataHandler/Interfaces/IFileHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoltageDataHandler.Interfaces
+{
+    internal interface IFileHandler
+    {
+        List<string> ReadFiles(string filePath);
+        void WriteFiles(string filePath, List<string> files);
+    }
+}
---
+++ VoltageDataHandler/VoltageDataHandler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VoltageDataHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
